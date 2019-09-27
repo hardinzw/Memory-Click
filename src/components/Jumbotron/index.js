18de97e679e522514dd5ba384afc304f733a3c5e
Zachary Hardin <hardinzw@gmail.com>
2019-09-27 19:32:34 -0400

    completed game, ready for deployment

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -5,8 +5,8 @@ function Jumbotron(props) {
     return (
         <div className="jumbotron jumbotron-fluid">
             <div className="container">
-                <h1 className="display-2">Fluid jumbotron</h1>
-                <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                <h1 className="display-2">Do You Have the Power?</h1>
+                <p className="lead">Click on an image to harness the power of Gray Skull, but don't click on any more than once!</p>
             </div>
         </div>
     );
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 const Navbar = props => (
   <div className="navbar ">
-    <div>Looney Memory</div>
+    <div>Masters of the Memory</div>
     <div className={props.navMsgColor}>{props.navMsg}</div>
     <div>
       Score: {props.score} <span className="divider">|</span> High Score: {props.highScore}
