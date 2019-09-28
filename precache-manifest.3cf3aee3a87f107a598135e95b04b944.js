1564efb6a3786244c147cb3568c4d5a8aa85b45c
Zachary Hardin <hardinzw@gmail.com>
2019-09-28 10:11:19 -0400

    Updates

---
+++ precache-manifest.3cf3aee3a87f107a598135e95b04b944.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7506050afa57203e1214665572c0ca37",
+    "url": "/Memory-Click/index.html"
+  },
+  {
+    "revision": "0475e968b32e05ebe811",
+    "url": "/Memory-Click/static/css/main.f508386b.chunk.css"
+  },
+  {
+    "revision": "dea4e683c52caf03df1e",
+    "url": "/Memory-Click/static/js/2.b03c78ff.chunk.js"
+  },
+  {
+    "revision": "0475e968b32e05ebe811",
+    "url": "/Memory-Click/static/js/main.55710077.chunk.js"
+  },
+  {
+    "revision": "ab93714f6c78d45713e0",
+    "url": "/Memory-Click/static/js/runtime-main.47757ef9.js"
+  }
+]);
