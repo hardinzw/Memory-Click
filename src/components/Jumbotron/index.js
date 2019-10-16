e7b0a29d691196149cf3b18fad7a9b9448fc3a53
Zachary Hardin <hardinzw@gmail.com>
2019-10-15 21:21:55 -0400

    edited for typo

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -6,7 +6,7 @@ function Jumbotron(props) {
         <div className="jumbotron jumbotron-fluid">
             <div className="container">
                 <h1 className="display-2">Do You Have the Power?</h1>
-                <p className="lead">Click on an image to harness the power of Gray Skull, but don't click on any more than once!</p>
+                <p className="lead">Click on an image to harness the power of Gray Skull, but don't click an image more than once!</p>
             </div>
         </div>
     );
